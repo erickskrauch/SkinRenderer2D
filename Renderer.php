8d8aabeda743aa1a429dc5032ff3676cc686b6cb
ErickSkrauch <erickskrauch@yandex.ru>
2014-10-11 11:11:31 +0300

    Try to make this PSR-0

--- Renderer.php
+++ Renderer.php
@@ -22,9 +22,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-namespace ErickSkrauch\Minecraft;
+namespace ErickSkrauch\SkinRenderer2D;
 
-class SkinRenderer2D {
+class Renderer {
     private $image = NULL;
     private $isAlpha = NULL;
     private $is1_8;
